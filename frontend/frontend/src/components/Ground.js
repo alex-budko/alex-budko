e127108cb958d115ce66ebea454329d4c2a2075f
alex-budko <xalex.budko@gmail.com>
2022-07-07 21:53:03 -0400

    texture map testing

--- frontend/frontend/src/components/Ground.js
+++ frontend/frontend/src/components/Ground.js
@@ -1,50 +1,65 @@
-import {Plane, MeshReflectorMaterial} from "@react-three/drei"
+import { MeshReflectorMaterial, useTexture} from "@react-three/drei"
 import { LinearEncoding, RepeatWrapping, TextureLoader } from "three"
 import { useLoader } from "@react-three/fiber"
 import { usePlane } from '@react-three/cannon'
 
 import React, { useEffect } from 'react'
 
-function Ground() {
-    const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0]}))
-    const [roughness, normal] = useLoader(TextureLoader, [
-        'textures/terrain-roughness.jpg',
-        'textures/terrain-normal.jpg',
+
+function Plane(props) {
+    let colorTexture = useTexture('S_Color.jpg')
+
+    const [roughness, normal, ao] = useLoader(TextureLoader, [
+        'S_Roughness.jpg',
+        'S_Normal.jpg',
+        'S_AO.jpg',
     ])
 
     useEffect(()=> {
-        [normal, roughness].forEach((tex)=> {
+        [normal, roughness, colorTexture].forEach((tex)=> {
             tex.wrapS = RepeatWrapping;
             tex.wrapT = RepeatWrapping;
-            tex.repeat.set(5, 5)
+            tex.repeat.set(100, 100)
         })
         normal.encoding = LinearEncoding
-    }, [normal, roughness])
+    }, [normal, roughness, colorTexture])
 
+    const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }))
     return (
-        <Plane ref={ref} castShadow receiveShadow args={[150, 150]} rotation-x={[-Math.PI * 0.5]}>
-            <MeshReflectorMaterial 
-                normalMap={normal}
-                roughnessMap={roughness}
-                envMapIntensity={0}
-                dilthering={true}
-                color={[0.0166, 0.0166, 0.0166]}
-                roughness={0.7}
-                blur={[1000, 400]}
-                mixBlur={30}
-                mixStrength={80}
-                mixContrast={1}
-                resolution={512}
-                mirror={0}
-                depthScale={0.01}
-                minDepthThreshold={0.9}
-                maxDepthThreshold={1}
-                depthToBlurRatioBias={0.25}
-                debug={0}
-                reflectorOffset={0.2}
-            />
-        </Plane>
+      <mesh ref={ref}>
+        <planeGeometry args={[1000, 1000]} />
+        <MeshReflectorMaterial 
+            normalMap={normal}
+            map={colorTexture}
+            // aoMap={ao}
+            roughnessMap={roughness}
+            envMapIntensity={0}
+            dilthering={true}
+            color={[1, 1, 1]}
+            roughness={0.7}
+            blur={[1000, 400]}
+            mixBlur={30}
+            mixStrength={80}
+            mixContrast={1}
+            resolution={512}
+            mirror={0}
+            depthScale={0.01}
+            minDepthThreshold={0.9}
+            maxDepthThreshold={1}
+            depthToBlurRatioBias={0.25}
+            debug={0}
+            reflectorOffset={0.2}
+        />
+      </mesh>
     )
 }
 
-export default Ground
+function Ground() {
+    
+
+    return (
+        <Plane castShadow receiveShadow args={[150, 150]} rotation-x={[-Math.PI * 0.5]} />
+    )
+}
+
+export default Ground;
--- frontend/frontend/src/components/HomeDrawing.js
+++ frontend/frontend/src/components/HomeDrawing.js
@@ -1,6 +1,8 @@
 import {Canvas} from "@react-three/fiber"
 import {PerspectiveCamera, OrbitControls,} from "@react-three/drei"
 import { angleToRads } from "../utils/angle"
+import { useBox } from '@react-three/cannon'
+
 import "../styles.css"
 
 import { Physics } from '@react-three/cannon'
@@ -9,8 +11,19 @@ import Ground from "./Ground"
 import Lights from "./Lights"
 import MyObject from "./MyObject"
 import TextMesh from './TextMesh'
-function HomeDrawing() {
 
+function Cube(props) {
+    const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }))
+    return (
+      <mesh receiveShadow castShadow ref={ref}>
+        <boxGeometry />
+        <meshLambertMaterial color="hotpink" />
+      </mesh>
+    )
+}
+
+function HomeDrawing() {
+    
     const orbitControlsRef = useRef(null)
     
     return (
@@ -18,18 +31,22 @@ function HomeDrawing() {
             <Canvas shadows >
                 <Suspense fallback={null}>
                     <ambientLight />
-                    <color args={[.5, .5, .5]} attach={"background"} />
+                    <color args={[1, 1, 1]} attach={"background"} />
                     <PerspectiveCamera fov={50} makeDefault position={[3, 2, 5]}/>
                     <OrbitControls ref={orbitControlsRef}  maxPolarAngle={angleToRads(80)} />
                     <Lights />
                     
                     <Physics>
                         <TextMesh />
-                        <MyObject />
+                        {/* <MyObject /> */}
+                        <Cube position={[0, 10, -1]} />
+                        <Cube position={[0, 20, -2]} />
+                        <Cube position={[0, 30, -1]} />
+                        <Cube position={[0, 25, -2]} />
+                        <Cube position={[0, 25, -1]} />
+                        <Cube position={[0, 32, -2]} />
                         <Ground />
                     </Physics>
-
-                    
                 </Suspense>
             </Canvas>
         </div>
